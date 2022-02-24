5ae650756461c82b9a5d7d1b19a293c5a15a6270
wyek1n <96416488+wyek1n@users.noreply.github.com>
2022-02-25 00:23:04 +0800

    Update transcode.sh

--- transcode.sh
+++ transcode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+TOKEN=422236727:AAEYbmEvLYGajn4-lYnmV_HgmAgJLCxCE4w
+chat_ID=430924615
+message_text="Video convertion is done!"
+MODE='HTML'
+URL="https://api.telegram.org/bot${TOKEN}/sendMessage"
+
 transcode(){
     Path_2=$1
 
@@ -10,7 +16,7 @@ transcode(){
     echo -n "$IFS" | hexdump
 
     echo "开始视频转码"
-    for  file_2 in $(find ${Path_2} -name "*.avi" -o -name "*.wmv" -o -name "*.mp4" -o -name "*.rmvb" -o -name "*.flv" -o -name "*.3gp")
+    for  file_2 in $(find ${Path_2} -name "*.avi" -o -name "*.wmv" -o -name "*.mp4" -o -name "*.rmvb" -o -name "*.flv" -o -name "*.3gp" -o -name "*.ts" -o -name "*.mpg" -o -name "*.dat" -o -name "*.rm" -o -name "*.mkv")
     do
         echo -n "$IFS" | hexdump
         echo "正在处理: ${file_2}"
@@ -27,11 +33,11 @@ transcode(){
         then
             echo "正在转码: ${file_2}"
             echo -n "$IFS" | hexdump
-            ffmpeg -i "${file_2}" -pix_fmt yuv420p "${file_2Dirname}/${file_2BasenameNoExtension}.mkv" -y
+            ffmpeg -i "${file_2}" -c:v libx264 -preset veryfast -b:v 500k -r 24 -c:a copy -y -hide_banner "${file_2Dirname}/${file_2BasenameNoExtension}_264.mp4" -y
             echo "${file_2} 转码完成"
             echo -n "$IFS" | hexdump
 
-            if test -e "${file_2Dirname}/${file_2BasenameNoExtension}.mkv"
+            if test -e "${file_2Dirname}/${file_2BasenameNoExtension}_264.mp4"
             then 
                 rm -rf "${file_2}"
                     echo "已删除原视频"
@@ -43,3 +49,5 @@ transcode(){
     IFS=${SAVEIFS}
     echo -n "$IFS" | hexdump
 }
+
+curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}"

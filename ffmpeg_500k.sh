a8ab4fb0c3ff6dbbdb18e81a0aa7c90a524c3c70
yuanxia688 <yuanxia688@outlook.com>
2022-02-26 23:40:59 +0800

    Add a new bash.

--- ffmpeg_500k.sh
+++ ffmpeg_500k.sh
@@ -10,33 +10,41 @@ URL="https://api.telegram.org/bot${TOKEN}/sendMessage"
 
 curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text_1}"
 
-SAVEIFS=${IFS}
+IFS=$'\n';
+echo -n "$IFS" | hexdump
 
-for  file_2 in $(find ${Path_2} -name "*.avi" -o -name "*.wmv" -o -name "*.mp4" -o -name "*.rmvb" -o -name "*.flv" -o -name "*.3gp" -o -name "*.ts" -o -name "*.mpg" -o -name "*.dat" -o -name "*.rm" -o -name "*.mkv")
+echo "开始视频转码"
+for  file_2 in $(find ${Path_2} -name "*.avi" -o -name "*.wmv" -o -name "*.mp4" -o -name "*.rmvb" -o -name "*.flv" -o -name "*.3gp")  # 这里填要查找的视频后缀
 do
-	echo "正在处理: ${file_2}"
-	file_2name=$(basename ${file_2})
-	echo "文件: ${file_2name}"
-	file_2Dirname=$(dirname ${file_2})
-	echo "路径: ${file_2Dirname}"
-	file_2Extension=${file_2name##*.}
-	echo "扩展名: ${file_2Extension}"
-	file_2BasenameNoExtension=${file_2name%.*}
-	echo "文件名: ${file_2BasenameNoExtension}"
+    echo -n "$IFS" | hexdump
+    echo "正在处理: ${file_2}"
+    file_2name=$(basename ${file_2})
+    echo "文件: ${file_2name}"
+    file_2Dirname=$(dirname ${file_2})
+    echo "路径: ${file_2Dirname}"
+    file_2Extension=${file_2name##*.}
+    echo "扩展名: ${file_2Extension}"
+    file_2BasenameNoExtension=${file_2name%.*}
+    echo "文件名: ${file_2BasenameNoExtension}"
 
-	if hash ffmpeg 2>/dev/null
-	then
-		echo "正在转码: ${file_2}"
-		ffmpeg -i "${file_2}" -c:v libx264 -preset veryfast -b:v 500k -r 24 -c:a copy -y -hide_banner "${file_2Dirname}/${file_2BasenameNoExtension}_264.mp4" -y
-		echo "${file_2} 转码完成"
-		if test -e "${file_2Dirname}/${file_2BasenameNoExtension}_264.mp4"
-		then 
-			rm -rf "${file_2}"
-			echo "已删除原视频"
-		fi
-	fi
-done
+    if hash ffmpeg 2>/dev/null
+    then
+        echo "正在转码: ${file_2}"
+        echo -n "$IFS" | hexdump
+        ffmpeg -i "${file_2}" -c:v libx264 -preset veryfast -b:v 500k -r 24 -c:a copy -y -hide_banner -pix_fmt yuv420p "${file_2Dirname}/${file_2BasenameNoExtension}.mkv" -y   # 这里填转码输出文件后缀和转码参数
+        echo "${file_2} 转码完成"
+        echo -n "$IFS" | hexdump
+
+        if test -e "${file_2Dirname}/${file_2BasenameNoExtension}.mkv"   # 同上，同步改后缀
+        then 
+            rm -rf "${file_2}"
+                echo "已删除原视频"
+        fi
+    echo -n "$IFS" | hexdump
+    fi
+done;
 
 IFS=${SAVEIFS}
+echo -n "$IFS" | hexdump
 
 curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text_2}"
